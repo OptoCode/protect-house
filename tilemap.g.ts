d3894c626a87eee9dd1974065ad68e42c1ae61d0
OptoCode <67488528+OptoCode@users.noreply.github.com>
2021-06-15 21:51:30 -0500

    Initial files for MakeCode project

---
+++ tilemap.g.ts
@@ -0,0 +1,35 @@
+// Auto-generated code. Do not edit.
+namespace myTiles {
+    //% fixedInstance jres blockIdentity=images._tile
+    export const transparency16 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile1 = image.ofBuffer(hex``);
+
+    helpers._registerFactory("tilemap", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "level1":
+            case "level1":return tiles.createTilemap(hex`0a0008000607070707070707070706090101010101010907060205050505050504080702050505050505040706020505050505050406060205050505050504080709030303030303090706060706060706060706`, img`
+. . . . . . . . . . 
+. 2 . . . . . . 2 . 
+. . . . . . . . . 2 
+. . 2 2 . . . . . . 
+. . 2 2 . . . . . . 
+. . . . . . . . . 2 
+. 2 . . . . . . 2 . 
+. . . . . . . . . . 
+`, [myTiles.transparency16,sprites.castle.tilePath2,sprites.castle.tilePath4,sprites.castle.tilePath8,sprites.castle.tilePath6,sprites.castle.tilePath5,sprites.castle.tileGrass1,sprites.castle.tileGrass3,sprites.builtin.forestTiles2,myTiles.tile1], TileScale.Sixteen);
+        }
+        return null;
+    })
+
+    helpers._registerFactory("tile", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "transparency16":return transparency16;
+            case "myTile":
+            case "tile1":return tile1;
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
